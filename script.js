60e74f6144126e2128f90a7379194659917cbc40
datallain07 <datff1900@gmail.com>
2025-08-14 00:08:19 +0700

    update

--- script.js
+++ script.js
@@ -133,12 +133,12 @@ document.addEventListener("DOMContentLoaded", function() {
       popup.style.opacity = "1";
     });
 
-    let countdown = 0;
+    let countdown = 5;
     closeBtn.disabled = true;
     closeBtn.style.opacity = "0.5";
     closeBtn.style.pointerEvents = "none";
     closeBtn.style.cursor = "not-allowed";
-    //closeBtn.textContent = `Có thể đóng sau ${countdown}s`;
+    closeBtn.textContent = `Có thể đóng sau ${countdown}s`;
 
     const countdownInterval = setInterval(() => {
       countdown--;
