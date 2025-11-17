b45a48c257cae16f7fea181c1b19e3c26d7ce38a
datallain07 <datff1900@gmail.com>
2025-11-17 18:53:57 +0700

    update

--- skinlist.js
+++ skinlist.js
@@ -214,17 +214,3 @@ document.getElementById("dlAndroidX").addEventListener("click", () => {
 document.getElementById("dlIOSX").addEventListener("click", () => {
   document.getElementById("dlPopupX").classList.remove("show");
 });
-
-document.addEventListener("DOMContentLoaded", () => {
-  const storedData = JSON.parse(localStorage.getItem("skinsList") || "[]");
-  
-  const currentList = skins.map((s) => `${s.champion}-${s.name}`);
-  
-  const newSkins = currentList.filter((s) => !storedData.includes(s));
-  
-  if (newSkins.length > 0) {
-    showToast(`Có thêm ${newSkins.length} mod mới🎉`);
-  }
-  
-  localStorage.setItem("skinsList", JSON.stringify(currentList));
-});
