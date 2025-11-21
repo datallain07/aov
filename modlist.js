2dca9002f78319f1e85827512b11ed96954df5ec
datallain07 <datff1900@gmail.com>
2025-11-21 14:29:37 +0700

    update

--- modlist.js
+++ modlist.js
@@ -1,5 +1,16 @@
 const skins = [
 {
+champion: "Bright",
+name: "Kaslana",
+label: "Mới nhất, 21thg11",
+Android: "https://link4m.com/JTAnzeH2",
+IOS: "https://link4m.com/EHD8L1",
+bgImg: "img/kaslana.png",
+miniImg: "img/kaslana_mini.png",
+desc: "Effects - Sound - Button",
+color: "255, 225, 75",
+},
+{
 champion: "Wiro",
 name: "Mặc định",
 label: "Cài 1 lần, dùng đến khi game sập",
