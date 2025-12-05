0b79f6691143b7498a77907c98690172a935c38a
datallain07 <datff1900@gmail.com>
2025-12-05 16:01:28 +0700

    update

--- modlist.js
+++ modlist.js
@@ -1,5 +1,15 @@
 const skins = [
 {
+champion: "Butterfly",
+name: "Cartethyia",
+label: "Mới nhất, tải lên ngày 5thg12",
+Android: "https://link4m.com/dALMLKkv",                               IOS: "https://link4m.com/xHgO8hWf",
+bgImg: "img/cartethyia.png",
+miniImg: "img/cartethyia_head.png",
+desc: "Effects - Sound - Button",
+color: "100, 181, 255",
+},
+{
 champion: "Wiro",
 name: "Mặc định",
 label: "Cài 1 lần, dùng đến khi game sập",
