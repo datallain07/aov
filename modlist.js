31169a251b161ffe6d6b7779fe1017e19e0c1307
datallain07 <datff1900@gmail.com>
2026-02-14 22:59:40 +0700

    update

--- modlist.js
+++ modlist.js
@@ -1,5 +1,16 @@
 const skins = [
 {
+champion: "Florentino",
+name: "Furina",
+label: "#Genshin Impact | 14thg2",
+Android: "https://link4m.com/gtdCQA",                                 IOS: "https://link4m.com/9MUjC",
+bgImg: "img/furina.png",
+miniImg: "img/furina_mini.png",
+videoPreview: "video/furina.mp4",
+desc: "Effects - Sound - Button",
+color: "50, 180, 255",
+},
+{
 champion: "Wiro",
 name: "Mặc định",
 label: "Cài 1 lần, dùng đến khi game sập",
