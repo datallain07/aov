d4f0f293378e9e25e40e88cb693b7269421abf43
datallain07 <datff1900@gmail.com>
2026-03-25 18:25:04 +0700

    update accounts.js

--- accounts.js
+++ accounts.js
@@ -1,4 +1,7 @@
 const accounts = [
   { username: "Đạt Allain", password: "2251225122832257115300560086", expire: "1/1/2100" },
-  { username: "Hiếu Gay", password: "5300720078228622762252007700380068007500", expire: "1/1/2100" }
+  { username: "Hiếu Gay", password: "5300720078228622762252007700380068007500", expire: "1/1/2100" },
+  { username: "Nguyễn Văn Doanh", password: "52007822862276228800660053004300", expire: "1/1/2100" },
+  { username: "Sinon", password: "008622512283228322260077004300660043", expire: "1/1/2100" },
+  { username: "Page Ngu", password: "7500680078228622762284005300750038004300", expire: "1/1/2100" }
 ];
