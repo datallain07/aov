a6fd83e2d0a76d8b77daada774f82bdb9bbf9497
datallain07 <datff1900@gmail.com>
2026-04-09 21:15:37 +0700

    update

--- mod.js
+++ mod.js
@@ -1,5 +1,17 @@
 const skins = [
 {
+champion: "Allain",
+name: "Firefly",
+label: "#Honkai: Star Rail | 9thg4",
+bgImg: "img/firefly.png",
+miniImg: "img/firefly_mini.png",
+videoPreview: "video/firefly.mp4",
+desc: "Effects - Sound - Button - Notify",
+color: "70, 255, 220",
+Android: "https://link4m.com/HP8hrEE",
+IOS: "https://link4m.com/HP8hrEE",
+},
+{
 champion: "Wiro",
 name: "Mặc định",
 label: "Cài 1 lần, dùng đến khi game sập",
